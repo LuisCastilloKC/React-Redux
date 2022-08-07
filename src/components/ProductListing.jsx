f56a23daf9a7a47780b935823397aa4ecc7544db
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-07 13:36:58 -0400

    add  async function with api and dispatch data to store

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { setProducts } from '../redux/actions/productActions'
 import ProductComponent from './ProductComponent'
 
 const ProductListing = () => {
     const products = useSelector((state)=> state)
+    const dispatch = useDispatch()
 
+        const fetchProducts = async () => {
+            const response = await axios.get("https://fakestoreapi.com/products").catch((error)=> {
+                console.log("error", error)
+            })
+            dispatch(setProducts(response.data))
+        }
+        useEffect(() => {
+            fetchProducts()
+        }, [])
+        
     console.log(products)
     return(
         <div>
