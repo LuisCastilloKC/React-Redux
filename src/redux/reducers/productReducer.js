33deafea4fac6a830b55e1240e52c5d5f352b320
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-06 22:41:55 -0400

    add product reducer with temporality initial state

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import { ActionTypes } from "../contants/action-types";
+
+// initializing a temporalty state
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Dipesh",
+      category: "programmer",
+    },
+  ],
+};
+
+//Reducer always take the initial State and the action. {type, payload} I'm destructor the action.
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
