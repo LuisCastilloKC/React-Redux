617543901b64d54950a693729bfc5aabb1dcea97
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-07 14:48:26 -0400

    add selectedProductReducer to combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 // here will have multiple reducer and with I will combine the reducer
 import { combineReducers } from 'redux';
-import { productReducer } from './productReducer'
+import { productReducer, selectedProductReducer } from './productReducer'
 
  const reducers = combineReducers({
-    allProducts: productReducer
+    allProducts: productReducer,
+    product: selectedProductReducer,
 })
 
 export default reducers
