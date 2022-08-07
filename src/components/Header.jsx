20937c4cc43f87273e245faba8d99ecc4cb5b9de
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-07 00:13:33 -0400

    add header and products component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    
+    return(
+        <div>
+            <div>
+                <h2>FakeShop</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header
