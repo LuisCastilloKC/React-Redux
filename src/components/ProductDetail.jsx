6f41e5f8da700bd443d8d6c4e9fdac68e518839b
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-07 14:43:51 -0400

    add selectedProduct Reducer

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectedProduct} from '../redux/actions/productActions'
 const ProductDetail = () => {
+    const product = useSelector((state) => state.product)
   const { productId } = useParams();
   const dispatch = useDispatch()
   console.log(productId);
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -2,14 +2,24 @@ import { ActionTypes } from "../contants/action-types";
 
 // initializing a temporality state
 const initialState = {
-  products: [ ],
+  products: [],
 };
 
 //Reducer always take the initial State and the action. {type, payload} I'm destructor the action.
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
-      return {...state, products: payload};
+      return { ...state, products: payload };
+    default:
+      return state;
+  }
+};
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+
     default:
       return state;
   }
