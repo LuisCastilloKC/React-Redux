649bbc4f4c860d376e59a611f1dd7b321c707cb3
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-07 00:39:06 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import ProductDetail from "./components/ProductDetail";
+import ProductListing from "./components/ProductListing";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+        <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route> 404 Not Found!</Route>
+      </Routes>
     </div>
   );
 }
